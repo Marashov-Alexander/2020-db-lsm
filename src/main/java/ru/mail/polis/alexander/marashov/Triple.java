004e0e54b5663e1b434e92bc0583a83d1e4bc324
Marashov_Alexander <sanYAMarashov>
2020-05-19 14:31:44 +0300

    Javadoc fix

--- src/main/java/ru/mail/polis/alexander/marashov/Triple.java
+++ src/main/java/ru/mail/polis/alexander/marashov/Triple.java
@@ -1,19 +1,17 @@
 package ru.mail.polis.alexander.marashov;
 
-/*
-* Class with three template objects
-*
-* */
+/**
+ * Class with three template objects
+ */
 public class Triple<F, S, T> {
 
     public F first;
     public S second;
     public T third;
 
-    /*
+    /**
      * Creates an object with three template objects
-     *
-     * */
+     */
     public Triple(final F first, final S second, final T third) {
         this.first = first;
         this.second = second;
